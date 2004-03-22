cd6d608599d7e1b02f90b3b21e3362bfc51fca19
jact <jact>
2004-03-22 19:45:26 +0000

    Initial revision

---
+++ shared/footer.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of OpenClinic
+ *
+ * Copyright (c) 2002-2004 jact
+ * Licensed under the GNU GPL. For full terms see the file LICENSE.
+ *
+ * $Id: footer.php,v 1.1 2004/03/22 19:47:22 jact Exp $
+ */
+
+/**
+ * footer.php
+ ********************************************************************
+ * Contains the common foot of the web pages
+ ********************************************************************
+ * Author: jact <jachavar@terra.es>
+ * Last modified: 22/03/04 20:47
+ */
+
+  if (str_replace("\\", "/", __FILE__) == $_SERVER['SCRIPT_FILENAME'])
+  {
+    header("Location: ../index.php");
+    exit();
+  }
+?>
+</div><!-- End #mainZone -->
+<!-- End Main Zone -->
+
+<!-- Footer -->
+<div id="footer">
+  <?php echo '<a href="../home/index.php" accesskey="1">' . _("Clinic Home") . '</a> |'; ?>
+
+  <a href="../index.html"><?php echo _("OpenClinic Readme"); ?></a> |
+
+  <a href="../doc/index.php?tab=<?php echo $tab; ?>&amp;nav=<?php echo $nav; ?>" title="<?php echo _("Opens a new window"); ?>" onclick="return popSecondary('../doc/index.php?tab=<?php echo $tab; ?>&amp;nav=<?php echo $nav; ?>')"><?php echo _("Help"); ?></a>
+
+  <?php
+    if ($_SESSION["userId"] == 1 || ($_SESSION["userId"] > 0 && $_SESSION["userId"] < 3 && !OPEN_DEMO))
+    {
+  ?>
+      | <a href="../shared/view_source.php?file=<?php echo $_SERVER['PHP_SELF']; ?>&amp;tab=<?php echo $tab; ?>" title="<?php echo _("Opens a new window"); ?>" onclick="return popSecondary('../shared/view_source.php?file=<?php echo $_SERVER['PHP_SELF']; ?>&amp;tab=<?php echo $tab; ?>')"><?php echo _("View source code"); ?></a>
+  <?php
+    }
+
+    if (defined("OPEN_DEMO") && OPEN_DEMO)
+    {
+      echo ' | <a href="../demo_version.html">' . _("Demo version features") . "</a>\n";
+      echo '<div class="error">' . _("This is a demo version") . "</div>\n";
+    }
+  ?>
+</div><!-- End #footer -->
+<!-- End Footer -->
+
+<!-- Sub Footer -->
+<div class="subFooter">
+  <?php
+    echo _("Powered by OpenClinic");
+    if (defined("OPEN_VERSION"))
+    {
+      echo ' ' . _("version") . ' ' . OPEN_VERSION;
+    }
+  ?>
+  <br />
+  Copyright &copy; 2002-2004 <a href="mailto:CUT-THIS.jachavar&#64;terra.es" accesskey="9">Jose Antonio Chavarría</a>
+  <br />
+  <?php echo _("under the"); ?>
+  <a href="../home/license.php">GNU General Public License</a>
+</div><!-- End .subFooter -->
+<!-- End Sub Footer -->
+</body>
+</html>
---
+++ shared/view_source.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file is part of OpenClinic
+ *
+ * Copyright (c) 2002-2004 jact
+ * Licensed under the GNU GPL. For full terms see the file LICENSE.
+ *
+ * $Id: view_source.php,v 1.1 2004/03/22 19:45:26 jact Exp $
+ */
+
+/**
+ * view_source.php
+ ********************************************************************
+ * View source code of a file screen
+ ********************************************************************
+ * Author: jact <jachavar@terra.es>
+ * Last modified: 22/03/04 20:45
+ */
+
+  require_once("../shared/read_settings.php");
+  require_once("../shared/login_check.php");
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/style.css" />
+
+<style type="text/css">
+<!--
+body {
+  background: #fff;
+  border: 0;
+  padding: 0;
+}
+-->
+</style>
+
+<title><?php echo _("Source file:") . ' ' . $_GET["file"]; ?></title>
+</head>
+<body
+<?php
+  if (count($_GET) == 0 || empty($_GET["file"]) || empty($_GET["tab"]))
+  {
+    echo 'onload="window.close()"';
+  }
+?>
+>
+<?php
+  if (isset($_SESSION["hasAdminAuth"]))
+  {
+    $file = basename($_GET["file"]);
+
+    if (is_file('../' . $_GET["tab"] . '/' . $file))
+    {
+      highlight_file('../' . $_GET["tab"] . '/' . $file);
+    }
+    elseif (is_file('../shared/' . $file))
+    {
+      highlight_file('../shared/' . $file);
+    }
+    else
+    {
+      echo '<p>' . _("No file found.") . "</p>\n";
+      echo '<p><a href="#" onclick="window.close(); return false;">' . _("Close Window") . "</a></p>\n";
+    }
+  }
+  else
+  {
+    echo sprintf(_("You are not authorized to use %s tab."), _("Admin")); // maybe change
+  }
+?>
+</body>
+</html>
