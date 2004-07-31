4b71d0afc974336c301aa4bbfe5cbe4aef575286
jact <jact>
2004-07-31 18:46:46 +0000

    Se utilizan las funciones showMessage() y showTable() y se usa la constante OPEN_SEPARATOR

--- medical/patient_fields.php
+++ medical/patient_fields.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2002-2004 jact
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
- * $Id: patient_fields.php,v 1.6 2004/07/07 17:22:50 jact Exp $
+ * $Id: patient_fields.php,v 1.7 2004/07/31 18:46:46 jact Exp $
  */
 
 /**
@@ -21,281 +21,198 @@
     header("Location: ../index.php");
     exit();
   }
+
+  $thead = array(
+    $title => array('colspan' => 2)
+  );
+
+  $tbody = array();
+
+  //$row = _("Last Update Date") . ":";
+  //$row .= OPEN_SEPARATOR;
+  //$row .= $postVars["last_update_date"];
+
+  //$tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="nif">' . _("Tax Identification Number (TIN)") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("nif", 20, 20, $postVars["nif"], $pageErrors["nif"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '* <label for="first_name">' . _("First Name") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("first_name", 25, 25, $postVars["first_name"], $pageErrors["first_name"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '* <label for="surname1">' . _("Surname 1") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("surname1", 30, 30, $postVars["surname1"], $pageErrors["surname1"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '* <label for="surname2">' . _("Surname 2") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("surname2", 30, 30, $postVars["surname2"], $pageErrors["surname2"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="address">' . _("Address") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlTextArea("address", 3, 30, $postVars["address"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="phone_contact">' . _("Phone Contact") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlTextArea("phone_contact", 3, 30, $postVars["phone_contact"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="sex">' . _("Sex") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+
+  $array = null;
+  $array['V'] = _("Male");
+  $array['H'] = _("Female");
+
+  $row .= htmlSelectArray("sex", $array, $postVars["sex"]);
+  unset($array);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="race">' . _("Race") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("race", 25, 25, $postVars["race"], $pageErrors["race"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="day">' . _("Birth Date") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+
+  $aux = explode("-", $postVars["birth_date"]);
+  $row .= htmlInputText("year", 4, 4, ((intval($aux[0]) != 0) ? intval($aux[0]) : ''));
+  $row .= " - ";
+  $row .= htmlInputText("month", 2, 2, ((intval($aux[1]) != 0) ? intval($aux[1]) : ''));
+  $row .= " - ";
+  $row .= htmlInputText("day", 2, 2, ((intval($aux[2]) != 0) ? intval($aux[2]) : ''));
+  $row .= " " . _("(yyyy-mm-dd)");
+  unset($aux);
+
+  if ($pageErrors["birth_date"] != "")
+  {
+    $row .= showMessage($pageErrors["birth_date"], OPEN_MSG_ERROR);
+  }
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="birth_place">' . _("Birth Place") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("birth_place", 40, 40, $postVars["birth_place"], $pageErrors["birth_place"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="dday">' . _("Decease Date") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+
+  $aux = explode("-", $postVars["decease_date"]);
+  $row .= htmlInputText("dyear", 4, 4, ((intval($aux[0]) != 0) ? intval($aux[0]) : ''));
+  $row .= " - ";
+  $row .= htmlInputText("dmonth", 2, 2, ((intval($aux[1]) != 0) ? intval($aux[1]) : ''));
+  $row .= " - ";
+  $row .= htmlInputText("dday", 2, 2, ((intval($aux[2]) != 0) ? intval($aux[2]) : ''));
+  $row .= " " . _("(yyyy-mm-dd)");
+  unset($aux);
+
+  if ($pageErrors["decease_date"] != "")
+  {
+    $row .= showMessage($pageErrors["decease_date"], OPEN_MSG_ERROR);
+  }
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="nts">' . _("Sanitary Card Number (SCN)") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("nts", 30, 30, $postVars["nts"], $pageErrors["nts"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="nss">' . _("National Health Service Number (NHSN)") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("nss", 30, 30, $postVars["nss"], $pageErrors["nss"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="family_situation">' . _("Family Situation") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlTextArea("family_situation", 3, 30, $postVars["family_situation"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="labour_situation">' . _("Labour Situation") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlTextArea("labour_situation", 3, 30, $postVars["labour_situation"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="education">' . _("Education") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlTextArea("education", 3, 30, $postVars["education"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="insurance_company">' . _("Insurance Company") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("insurance_company", 30, 30, $postVars["insurance_company"], $pageErrors["insurance_company"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '<label for="collegiate_number">' . _("Doctor you are assigned to") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+
+  $staffQ = new Staff_Query();
+  $staffQ->connect();
+  if ($staffQ->isError())
+  {
+    showQueryError($staffQ);
+  }
+
+  $numRows = $staffQ->selectType('D');
+  if ($staffQ->isError())
+  {
+    $staffQ->close();
+    showQueryError($staffQ);
+  }
+
+  $array = null;
+  $array[""] = ""; // to permit null value
+  if ($numRows)
+  {
+    while ($staff = $staffQ->fetch())
+    {
+      $array[$staff->getCollegiateNumber()] = $staff->getFirstName() . " " . $staff->getSurname1() . " " . $staff->getSurname2();
+    }
+    $staffQ->freeResult();
+  }
+  $staffQ->close();
+  unset($staffQ);
+
+  $row .= htmlSelectArray("collegiate_number", $array, $postVars["collegiate_number"]);
+  unset($array);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $tfoot = array(
+    htmlInputButton("button1", _("Submit"))
+    . htmlInputButton("button2", _("Reset"), "reset")
+    . htmlInputButton("return", _("Return"), "button", 'onclick="parent.location=\'' . $returnLocation . '\'"')
+  );
+
+  $options = array(
+    'shaded' => false,
+    'tfoot' => array('align' => 'center')
+  );
+
+  showTable($thead, $tbody, $tfoot, $options);
 ?>
-
-<table>
-  <thead>
-    <tr>
-      <th colspan="2">
-        <?php echo $title; ?>
-      </th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <!--tr>
-      <td-->
-        <?php //echo _("Last Update Date") . ":"; ?>
-      <!--/td>
-
-      <td-->
-        <?php //echo $postVars["last_update_date"]; ?>
-      <!--/td>
-    </tr-->
-
-    <tr>
-      <td>
-        <label for="nif"><?php echo _("Tax Identification Number (TIN)") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("nif", 20, 20, $postVars["nif"], $pageErrors["nif"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        * <label for="first_name"><?php echo _("First Name") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("first_name", 25, 25, $postVars["first_name"], $pageErrors["first_name"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        * <label for="surname1"><?php echo _("Surname 1") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("surname1", 30, 30, $postVars["surname1"], $pageErrors["surname1"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        * <label for="surname2"><?php echo _("Surname 2") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("surname2", 30, 30, $postVars["surname2"], $pageErrors["surname2"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="address"><?php echo _("Address") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showTextArea("address", 3, 30, $postVars["address"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="phone_contact"><?php echo _("Phone Contact") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showTextArea("phone_contact", 3, 30, $postVars["phone_contact"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="sex"><?php echo _("Sex") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php
-          $array = null;
-          $array['V'] = _("Male");
-          $array['H'] = _("Female");
-
-          showSelectArray("sex", $array, $postVars["sex"]);
-          unset($array);
-        ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="race"><?php echo _("Race") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("race", 25, 25, $postVars["race"], $pageErrors["race"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="day"><?php echo _("Birth Date") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php
-          $aux = explode("-", $postVars["birth_date"]);
-          showInputText("year", 4, 4, ((intval($aux[0]) != 0) ? intval($aux[0]) : ''));
-          echo " - ";
-          showInputText("month", 2, 2, ((intval($aux[1]) != 0) ? intval($aux[1]) : ''));
-          echo " - ";
-          showInputText("day", 2, 2, ((intval($aux[2]) != 0) ? intval($aux[2]) : ''));
-          echo " " . _("(yyyy-mm-dd)");
-          unset($aux);
-
-          if ($pageErrors["birth_date"] != "")
-          {
-            echo '<br /><span class="error">' . $pageErrors["birth_date"] . "</span>\n";
-          }
-        ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="birth_place"><?php echo _("Birth Place") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("birth_place", 40, 40, $postVars["birth_place"], $pageErrors["birth_place"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="dday"><?php echo _("Decease Date") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php
-          $aux = explode("-", $postVars["decease_date"]);
-          showInputText("dyear", 4, 4, ((intval($aux[0]) != 0) ? intval($aux[0]) : ''));
-          echo " - ";
-          showInputText("dmonth", 2, 2, ((intval($aux[1]) != 0) ? intval($aux[1]) : ''));
-          echo " - ";
-          showInputText("dday", 2, 2, ((intval($aux[2]) != 0) ? intval($aux[2]) : ''));
-          echo " " . _("(yyyy-mm-dd)");
-          unset($aux);
-
-          if ($pageErrors["decease_date"] != "")
-          {
-            echo '<br /><span class="error">' . $pageErrors["decease_date"] . "</span>\n";
-          }
-        ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="nts"><?php echo _("Sanitary Card Number (SCN)") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("nts", 30, 30, $postVars["nts"], $pageErrors["nts"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="nss"><?php echo _("National Health Service Number (NHSN)") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("nss", 30, 30, $postVars["nss"], $pageErrors["nss"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="family_situation"><?php echo _("Family Situation") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showTextArea("family_situation", 3, 30, $postVars["family_situation"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="labour_situation"><?php echo _("Labour Situation") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showTextArea("labour_situation", 3, 30, $postVars["labour_situation"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="education"><?php echo _("Education") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showTextArea("education", 3, 30, $postVars["education"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="insurance_company"><?php echo _("Insurance Company") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("insurance_company", 30, 30, $postVars["insurance_company"], $pageErrors["insurance_company"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        <label for="collegiate_number"><?php echo _("Doctor you are assigned to") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php
-          $staffQ = new Staff_Query();
-          $staffQ->connect();
-          if ($staffQ->isError())
-          {
-            showQueryError($staffQ);
-          }
-
-          $numRows = $staffQ->selectType('D');
-          if ($staffQ->isError())
-          {
-            $staffQ->close();
-            showQueryError($staffQ);
-          }
-
-          $array = null;
-          $array[""] = ""; // to permit null value
-          if ($numRows)
-          {
-            while ($row = $staffQ->fetch())
-            {
-              $array[$row->getCollegiateNumber()] = $row->getFirstName() . " " . $row->getSurname1() . " " . $row->getSurname2();
-            }
-            $staffQ->freeResult();
-          }
-          $staffQ->close();
-          unset($staffQ);
-
-          showSelectArray("collegiate_number", $array, $postVars["collegiate_number"]);
-          unset($array);
-        ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td class="center" colspan="2">
-        <?php
-          showInputButton("button1", _("Submit"));
-          showInputButton("button2", _("Reset"), "reset");
-          showInputButton("return", _("Return"), "button", 'onclick="parent.location=\'' . $returnLocation . '\'"');
-        ?>
-      </td>
-    </tr>
-  </tbody>
-</table>
