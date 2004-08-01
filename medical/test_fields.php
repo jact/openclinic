64a352f0b1496aeb3f583147fe6043dc99adcaf5
jact <jact>
2004-08-01 09:31:51 +0000

    Se utilizan las funciones showMessage() y showTable() y se usa la constante OPEN_SEPARATOR

--- medical/test_fields.php
+++ medical/test_fields.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2002-2004 jact
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
- * $Id: test_fields.php,v 1.2 2004/04/24 14:52:15 jact Exp $
+ * $Id: test_fields.php,v 1.3 2004/08/01 09:31:51 jact Exp $
  */
 
 /**
@@ -21,62 +21,49 @@
     header("Location: ../index.php");
     exit();
   }
+
+  $thead = array(
+    $title => array('colspan' => 2)
+  );
+
+  $tbody = array();
+
+  $row = '<label for="document_type">' . _("Document Type") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+  $row .= htmlInputText("document_type", 40, 128, $postVars["document_type"], $pageErrors["document_type"]);
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $row = '* <label for="path_filename">' . _("Path Filename") . ":" . "</label>\n";
+  $row .= OPEN_SEPARATOR;
+
+  //$row .= htmlInputHidden("MAX_FILE_SIZE", "70000");
+  $len = strlen($postVars["path_filename"]);
+  if ($len > 0)
+  {
+    $row .= htmlInputText("previous", $len, $len, $postVars['path_filename'], "", "text", true);
+    $row .= "<br />\n";
+  }
+
+  $row .= htmlInputFile("path_filename", $postVars['path_filename'], 50);
+
+  if (isset($pageErrors["path_filename"]))
+  {
+    $row .= htmlMessage($pageErrors["path_filename"], OPEN_MSG_ERROR);
+  }
+
+  $tbody[] = explode(OPEN_SEPARATOR, $row);
+
+  $tfoot = array(
+    htmlInputButton("button1", _("Submit"))
+    . htmlInputButton("button2", _("Reset"), "reset")
+    . htmlInputButton("return", _("Return"), "button", 'onclick="parent.location=\'' . $returnLocation . '\'"')
+  );
+
+  $options = array(
+    'shaded' => false,
+    'tfoot' => array('align' => 'center')
+  );
+
+  showTable($thead, $tbody, $tfoot, $options);
 ?>
-
-<table>
-  <thead>
-    <tr>
-      <th colspan="2">
-        <?php echo $title; ?>
-      </th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <tr>
-      <td>
-        <label for="document_type"><?php echo _("Document Type") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php showInputText("document_type", 40, 128, $postVars["document_type"], $pageErrors["document_type"]); ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td>
-        * <label for="path_filename"><?php echo _("Path Filename") . ":"; ?></label>
-      </td>
-
-      <td>
-        <?php
-          //showInputHidden("MAX_FILE_SIZE", "70000");
-
-          $len = strlen($postVars["path_filename"]);
-          if ($len > 0)
-          {
-            showInputText("previous", $len, $len, $postVars['path_filename'], "", "text", true);
-            echo "<br />\n";
-          }
-
-          showInputFile("path_filename", $postVars['path_filename'], 50);
-
-          if (isset($pageErrors["path_filename"]))
-          {
-            echo '<br /><span class="error">' . $pageErrors["path_filename"] . "</span>\n";
-          }
-        ?>
-      </td>
-    </tr>
-
-    <tr>
-      <td class="center" colspan="2">
-        <?php
-          showInputButton("button1", _("Submit"));
-          showInputButton("button2", _("Reset"), "reset");
-          showInputButton("return", _("Return"), "button", 'onclick="parent.location=\'' . $returnLocation . '\'"');
-        ?>
-      </td>
-    </tr>
-  </tbody>
-</table>
