b3785fd25e8511289b9dbcb21492d1243bffd316
jact <jact>
2004-08-06 12:01:14 +0000

    Ya no hace falta marcar el campo Tema

--- admin/user_fields.php
+++ admin/user_fields.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2002-2004 jact
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
- * $Id: user_fields.php,v 1.5 2004/08/02 11:18:46 jact Exp $
+ * $Id: user_fields.php,v 1.6 2004/08/06 12:01:14 jact Exp $
  */
 
 /**
@@ -94,7 +94,7 @@
     $tbody[] = explode(OPEN_SEPARATOR, $row);
   }
 
-  $row = '* <label for="id_theme">' . _("Theme") . ":" . "</label>\n";
+  $row = '<label for="id_theme">' . _("Theme") . ":" . "</label>\n";
   $row .= OPEN_SEPARATOR;
   $row .= htmlSelect("theme_tbl", "id_theme", $postVars["id_theme"], "theme_name");
 
