082b83e13d61d9d393c90d34aacd74b12d1ddbce
jact <jact>
2004-09-23 18:54:52 +0000

    Se tiene en cuenta la constante OPEN_BUFFER para usar funciones ob_*() o no

--- shared/xhtml_start.php
+++ shared/xhtml_start.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2002-2004 jact
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
- * $Id: xhtml_start.php,v 1.4 2004/09/22 18:20:49 jact Exp $
+ * $Id: xhtml_start.php,v 1.5 2004/09/23 18:54:52 jact Exp $
  */
 
 /**
@@ -29,6 +29,18 @@
     $contentType = "text/html";
   }
   $contentType .= "; charset=" . OPEN_CHARSET;
+
+  if (defined("OPEN_BUFFER") && OPEN_BUFFER)
+  {
+    if (eregi("gzip", $_SERVER['HTTP_ACCEPT_ENCODING']))
+    {
+      ob_start("ob_gzhandler");
+    }
+    else
+    {
+      ob_start();
+    }
+  }
 
   // To prevent 'short_open_tag = On' mistake
   echo '<?xml version="1.0" encoding="' . OPEN_ENCODING . '" standalone="no" ?>' . "\n";
