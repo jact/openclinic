c3e7bc6e2de8cf5b04a662c5790054b36f47417e
jact <jact>
2005-06-13 19:02:08 +0000

    Se cambian comentarios de la cabecera del archivo

--- shared/session_info.php
+++ shared/session_info.php
@@ -2,18 +2,18 @@
 /**
  * This file is part of OpenClinic
  *
- * Copyright (c) 2002-2004 jact
+ * Copyright (c) 2002-2005 jact
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
- * $Id: session_info.php,v 1.4 2004/10/18 17:24:05 jact Exp $
+ * $Id: session_info.php,v 1.5 2005/06/13 19:02:08 jact Exp $
  */
 
 /**
  * session_info.php
- ********************************************************************
+ *
  * Making session user info available on all pages
- ********************************************************************
- * Author: jact <jachavar@terra.es>
+ *
+ * Author: jact <jachavar@gmail.com>
  * @since 0.7
  */
 
