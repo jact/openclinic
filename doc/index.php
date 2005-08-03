fddf400723992b28813e5ddec06e258a9cb31892
jact <jact>
2005-08-03 17:04:45 +0000

    Se actualiza la estructura de los enlaces en .linkList

--- doc/index.php
+++ doc/index.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2002-2005 jact
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
- * $Id: index.php,v 1.14 2005/07/21 16:56:07 jact Exp $
+ * $Id: index.php,v 1.15 2005/08/03 17:04:45 jact Exp $
  */
 
 /**
@@ -62,11 +62,10 @@
 
 <!-- Side Bar -->
 <div id="sideBar">
-  <div class="linkList">
-    <span class="selected"><?php echo _("Help Topic"); ?></span>
-    <span class="noPrint"> | </span>
-    <a href="#"><?php echo _("Help Topic"); ?></a>
-  </div><!-- End .linkList -->
+  <ul class="linkList">
+    <li class="selected"><?php echo _("Help Topic"); ?></li>
+    <li><a href="#"><?php echo _("Help Topic"); ?></a></li>
+  </ul><!-- End .linkList -->
 
   <hr />
 
