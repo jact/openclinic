b928b63d0680b34aec226187bd99bfba4322d901
jact <jact>
2005-08-15 16:39:39 +0000

    $tab y $nav se quitan porque este fichero no saca salida HTML

--- medical/problem_edit.php
+++ medical/problem_edit.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2002-2005 jact
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
- * $Id: problem_edit.php,v 1.11 2005/07/30 15:10:25 jact Exp $
+ * $Id: problem_edit.php,v 1.12 2005/08/15 16:39:39 jact Exp $
  */
 
 /**
@@ -16,20 +16,18 @@
  * Author: jact <jachavar@gmail.com>
  */
 
-  ////////////////////////////////////////////////////////////////////
-  // Checking for post vars. Go back to medical problems list if none found.
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Checking for post vars. Go back to medical problems list if none found.
+   */
   if (count($_POST) == 0)
   {
     header("Location: ../medical/problem_list.php");
     exit();
   }
 
-  ////////////////////////////////////////////////////////////////////
-  // Controlling vars
-  ////////////////////////////////////////////////////////////////////
-  $tab = "medical";
-  $nav = "problems";
+  /**
+   * Controlling vars
+   */
   $onlyDoctor = false;
 
   require_once("../shared/read_settings.php");
@@ -37,31 +35,31 @@
   require_once("../classes/Problem_Page_Query.php");
   require_once("../shared/record_log.php"); // record log
 
-  ////////////////////////////////////////////////////////////////////
-  // Retrieving post vars
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Retrieving post vars
+   */
   $idPatient = intval($_POST["id_patient"]);
   $idProblem = intval($_POST["id_problem"]);
 
-  $errorLocation = "../medical/problem_edit_form.php?key=" . $idProblem . "&pat=" . $idPatient;
+  $errorLocation = "../medical/problem_edit_form.php?key=" . $idProblem . "&pat=" . $idPatient; // controlling var
 
-  ////////////////////////////////////////////////////////////////////
-  // Validate data
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Validate data
+   */
   $problem = new Problem();
 
   $problem->setIdProblem($_POST["id_problem"]);
 
   require_once("../medical/problem_validate_post.php");
 
-  ////////////////////////////////////////////////////////////////////
-  // Prevent user from aborting script
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Prevent user from aborting script
+   */
   $oldAbort = ignore_user_abort(true);
 
-  ////////////////////////////////////////////////////////////////////
-  // Update problem
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Update problem
+   */
   $problemQ = new Problem_Page_Query();
   $problemQ->connect();
   if ($problemQ->isError())
@@ -79,27 +77,27 @@
   $problemQ->close();
   unset($problemQ);
 
-  ////////////////////////////////////////////////////////////////////
-  // Record log process
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Record log process
+   */
   recordLog("Problem_Page_Query", "UPDATE", array($idProblem));
 
-  ////////////////////////////////////////////////////////////////////
-  // Reset abort setting
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Reset abort setting
+   */
   ignore_user_abort($oldAbort);
 
-  ////////////////////////////////////////////////////////////////////
-  // Destroy form values and errors
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Destroy form values and errors
+   */
   unset($_SESSION["postVars"]);
   unset($_SESSION["pageErrors"]);
 
-  $returnLocation = "../medical/problem_list.php?key=" . $idPatient;
+  $returnLocation = "../medical/problem_list.php?key=" . $idPatient; // controlling var
 
-  ////////////////////////////////////////////////////////////////////
-  // Redirect to problem list to avoid reload problem
-  ////////////////////////////////////////////////////////////////////
+  /**
+   * Redirect to $returnLocation to avoid reload problem
+   */
   $info = urlencode($problem->getWording()) . ($problem->getClosingDate(false) ? "&closed=Y" : "");
   unset($problem);
   header("Location: " . $returnLocation . "&updated=Y&info=" . $info);
