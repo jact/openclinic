8a17408bcf6eaa0b7c61d421e67b96d7f829acaa
jact <jact>
2006-08-17 17:40:29 +0000

    Se actualiza la cabecera informativa del fichero

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 /**
- * @package OpenClinic
- *
- * @copyright Copyright (c) 2002-2006 jact
- * @license Licensed under the GNU GPL. For full terms see the file LICENSE.
- *
- * $Id: index.php,v 1.5 2006/08/17 17:37:57 jact Exp $
- */
-
-/**
  * index.php
  *
  * Index page of the project
  *
- * @author jact <jachavar@gmail.com>
- * @todo i18n and HTML.php inclusion
+ * Licensed under the GNU GPL. For full terms see the file LICENSE.
+ *
+ * @package   OpenClinic
+ * @copyright 2002-2006 jact
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @version   CVS: $Id: index.php,v 1.6 2006/08/17 17:40:29 jact Exp $
+ * @author    jact <jachavar@gmail.com>
+ * @todo      i18n and HTML.php inclusion
  */
 
   // Ensuring a minimum version of PHP
