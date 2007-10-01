befe7234e8d7d2b88cd2fe5d1ab827ea47bf616a
jact <jact>
2007-10-01 20:00:08 +0000

    Se quita la llamada a Form::compareToken porque ya se hace en patient_validate_post.php (si se llama 2 veces, en la segunda llamada se crea otro token distinto al haberse borrado tras la primera llamada)

--- medical/patient_new.php
+++ medical/patient_new.php
@@ -7,9 +7,9 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2006 jact
+ * @copyright 2002-2007 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: patient_new.php,v 1.15 2006/12/14 22:41:18 jact Exp $
+ * @version   CVS: $Id: patient_new.php,v 1.16 2007/10/01 20:00:08 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -30,10 +30,6 @@
 
   require_once("../config/environment.php");
   require_once("../auth/login_check.php");
-  require_once("../lib/Form.php");
-
-  Form::compareToken('../medical/patient_new_form.php');
-
   require_once("../model/Patient_Page_Query.php");
   require_once("../shared/record_log.php"); // record log
 
