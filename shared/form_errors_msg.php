bfe719c9a3649c215737ecfddd3e4c127bc1c2c5
jact <jact>
2007-10-17 19:17:09 +0000

    Se sustituyen los manejadores de eventos por JS discreto (clase + .js)

--- shared/form_errors_msg.php
+++ shared/form_errors_msg.php
@@ -7,14 +7,16 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2006 jact
+ * @copyright 2002-2007 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: form_errors_msg.php,v 1.10 2006/09/30 17:26:59 jact Exp $
+ * @version   CVS: $Id: form_errors_msg.php,v 1.11 2007/10/17 19:17:09 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
   if (isset($formError) && count($formError) > 0)
   {
+    echo HTML::insertScript('target_focus.js');
+
     HTML::start('div', array('class' => 'error'));
     HTML::para(_("ERROR: Some fields have been incorrectly filled. Please fix the fields and send the form again. Each incorrectly filled field is marked with specific error message."));
 
@@ -24,7 +26,7 @@
       if ($value)
       {
         $array[] = HTML::strLink($value, '#' . $key, null,
-          array('onclick' => "document.getElementById('" . $key . "').focus();")
+          array('class' => 'target') // unobtrusive JS
         );
       }
     }
