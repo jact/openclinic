9664fc67f6e38f68f0b52812499805305da64609
jact <jact>
2007-10-28 19:04:28 +0000

    Sustituye a model/Page_Query.php

---
+++ model/Query/Page.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Page.php
+ *
+ * Contains the class Query_Page (pagination methods)
+ *
+ * Licensed under the GNU GPL. For full terms see the file LICENSE.
+ *
+ * @package   OpenClinic
+ * @copyright 2002-2007 jact
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @version   CVS: $Id: Page.php,v 1.1 2007/10/28 19:04:28 jact Exp $
+ * @author    jact <jachavar@gmail.com>
+ * @author    Jorge López Herranz <lopez.herranz@gmail.com>
+ */
+
+require_once(dirname(__FILE__) . "/../Query.php");
+
+/**
+ * Query_Page contains pagination methods and properties
+ *
+ * Methods:
+ *  void setItemsPerPage(int $value)
+ *  int getCurrentRow(void)
+ *  int getRowCount(void)
+ *  int getPageCount(void)
+ *  void _resetStats(int $page)
+ *  void _incrementRow(void)
+ *  void _calculateStats(int $rowCount, int $limitFrom = 0)
+ *
+ * @package OpenClinic
+ * @author jact <jachavar@gmail.com>
+ * @author Jorge López Herranz <lopez.herranz@gmail.com>
+ * @access public
+ * @see OPEN_SETTING_ITEMS_PER_PAGE
+ * @since 0.8
+ */
+class Query_Page extends Query
+{
+  // (These variables are 'protected'. Only can be used by inherited classes)
+  var $_itemsPerPage = OPEN_SETTING_ITEMS_PER_PAGE;
+  var $_rowNumber = 0;
+  var $_currentRow = 0;
+  var $_currentPage = 0;
+  var $_rowCount = 0;
+  var $_pageCount = 0;
+
+  /**
+   * void setItemsPerPage(int $value)
+   *
+   * @param int $value
+   * @access public
+   */
+  function setItemsPerPage($value)
+  {
+    $this->_itemsPerPage = intval($value);
+  }
+
+  /**
+   * int getCurrentRow(void)
+   *
+   * @return int
+   * @access public
+   */
+  function getCurrentRow()
+  {
+    return intval($this->_currentRow);
+  }
+
+  /**
+   * int getRowCount(void)
+   *
+   * @return int
+   * @access public
+   */
+  function getRowCount()
+  {
+    return intval($this->_rowCount);
+  }
+
+  /**
+   * int getPageCount(void)
+   *
+   * @return int
+   * @access public
+   */
+  function getPageCount()
+  {
+    return intval($this->_pageCount);
+  }
+
+  /**
+   * void _resetStats(int $page)
+   *
+   * @param int $page
+   * @access private
+   */
+  function _resetStats($page)
+  {
+    $this->_rowNumber = 0;
+    $this->_currentRow = 0;
+    $this->_currentPage = ($page > 1) ? intval($page) : 1;
+    $this->_rowCount = 0;
+    $this->_pageCount = 0;
+  }
+
+  /**
+   * void _incrementRow(void)
+   *
+   * @access private
+   */
+  function _incrementRow()
+  {
+    $this->_rowNumber = $this->_rowNumber + 1;
+    $this->_currentRow = $this->_rowNumber + (($this->_currentPage - 1) * $this->_itemsPerPage);
+  }
+
+  /**
+   * void _calculateStats(int $rowCount, int $limitFrom = 0)
+   *
+   * Calculate stats based on row count
+   *
+   * @param int $rowCount
+   * @param int $limitFrom (optional)
+   * @access private
+   */
+  function _calculateStats($rowCount, $limitFrom = 0)
+  {
+    $this->_rowCount = $rowCount;
+    if ($limitFrom > 0 && $limitFrom < $this->_rowCount)
+    {
+      $this->_rowCount = $limitFrom;
+    }
+    $this->_pageCount = (intval($this->_itemsPerPage) > 0) ? ceil($this->_rowCount / $this->_itemsPerPage) : 1;
+  }
+} // end class
+?>
