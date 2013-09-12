74d3d1bf4d31a9b4dfb01e274f921f2cc341d0c5
Jose Antonio Chavarría <jachavar@gmail.com>
2013-09-12 12:33:04 +0200

    Se modifica el valor de la constante OPEN_DB_SCHEMA_VERSION por el cambio de versión (0.8.20130912 aka 0.8.1)

--- config/global_constants.php
+++ config/global_constants.php
@@ -34,7 +34,7 @@
   define("OPEN_DEMO",               false);
   define("OPEN_SQL_DEBUG",          false && OPEN_DEBUG); // if true, trace of SQL sentences
   define("OPEN_BUFFER",             false && !OPEN_DEBUG); // if true, use ob_start(), ob_end_flush() functions
-  define("OPEN_DB_SCHEMA_VERSION",  "0.8.20130107"); // expected database schema version
+  define("OPEN_DB_SCHEMA_VERSION",  "0.8.20130912"); // expected database schema version
   define("OPEN_XML_ACTIVED",        false); // if true and is possible, application/xhtml+xml, otherwise text/html
   define("OPEN_MAX_LOGIN_ATTEMPTS", 3); // if zero, no limit login attempts
 
