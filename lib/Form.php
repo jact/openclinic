fddbfd01a538b4bb408b358710ec3786a5d8dd9c
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 23:56:03 +0200

    Inicialización de array antes de asignación (PHP7) (#9)
    Comprobación de array antes de consultar con count (PHP7) (#9)

--- lib/Form.php
+++ lib/Form.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2013 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: Form.php,v 1.27 2013/01/07 18:38:35 jact Exp $
  * @author    jact <jachavar@gmail.com>
  * @todo more helpers (dates, files...)
  */
@@ -379,6 +378,9 @@ class Form
    */
   public static function button($name, $value = null, $attribs = "")
   {
+    if ($attribs == "") {
+      $attribs = array();
+    }
     $attribs['id']    = isset($attribs['id']) ? $attribs['id'] : $name;
     $attribs['name']  = $name;
     $attribs['value'] = htmlspecialchars($value);
@@ -574,7 +576,7 @@ class Form
       }
     }
 
-    if (count($foot) > 0)
+    if (is_array($foot) && count($foot) > 0)
     {
       $_footText = '';
       foreach ($foot as $_row)
@@ -743,4 +745,3 @@ class Form
     }
   }
 } // end class
-?>
