1cd202bd70067895a9faa6352f4856fb7e5454ba
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 23:56:14 +0200

    Inicialización de array antes de asignación (PHP7) (#9)
    Comprobación de array antes de consultar con count (PHP7) (#9)

--- lib/HTML.php
+++ lib/HTML.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2013 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: HTML.php,v 1.19 2013/01/07 18:34:22 jact Exp $
  * @author    jact <jachavar@gmail.com>
  * @todo rename file to Html.php
  */
@@ -246,7 +245,7 @@ class HTML
       }
     }
 
-    if (count($foot) > 0)
+    if (is_array($foot) && count($foot) > 0)
     {
       $_html .= self::start('tfoot') . PHP_EOL;
       foreach ($foot as $_row)
@@ -438,6 +437,7 @@ class HTML
     $_query = '';
     if (is_array($arg))
     {
+      $_query = array();
       foreach ($arg as $_key => $_value)
       {
         $_query[] = urlencode($_key) . '=' . urlencode($_value);
@@ -621,4 +621,3 @@ class HTML
     return $_html;
   }
 } // end class
-?>
