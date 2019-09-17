67d2580340b4a2ee21b34db9e5690bdcb801f1e5
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 22:04:20 +0200

    Cambia el nombre del constructor de la clase (PHP7) (#9)

--- model/Setting.php
+++ model/Setting.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2013 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: Setting.php,v 1.18 2013/01/19 10:26:23 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -68,7 +67,7 @@ class Setting
 
   private $_trans; // to translate htmlspecialchars()
 
-  public function Setting()
+  public function __construct()
   {
     $this->_trans = array_flip(get_html_translation_table(HTML_SPECIALCHARS));
   }
@@ -380,4 +379,3 @@ class Setting
     return __CLASS__;
   }
 } // end class
-?>
