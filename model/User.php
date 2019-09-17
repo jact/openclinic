b027e9d15e75f4a983f1cdef2650c0040469f3a1
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 22:05:08 +0200

    Cambia el nombre del constructor de la clase (PHP7) (#9)

--- model/User.php
+++ model/User.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2013 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: User.php,v 1.14 2013/01/20 12:48:10 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -65,7 +64,7 @@ class User
 
   private $_trans; // to translate htmlspecialchars()
 
-  public function User()
+  public function __construct()
   {
     $this->_trans = array_flip(get_html_translation_table(HTML_SPECIALCHARS));
   }
@@ -423,4 +422,3 @@ class User
     return $this->getLogin();
   }
 } // end class
-?>
