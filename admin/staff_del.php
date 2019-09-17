f64f8ed0a6cc32d38e589a3c2bfb8b70bc18e0b4
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 23:51:54 +0200

    La clase Error cambia de nombre a AppError porque en PHP7 hay una clase predefinida con ese mismo nombre (#9)

--- admin/staff_del.php
+++ admin/staff_del.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2013 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: staff_del.php,v 1.20 2013/05/01 08:30:31 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -61,7 +60,7 @@
   if ( !$staff )
   {
     $staffQ->close();
-    Error::fetch($staffQ);
+    AppError::fetch($staffQ);
   }
 
   $staffQ->delete($staff->getIdMember(), $staff->getIdUser());
@@ -77,4 +76,3 @@
    * Redirect to $returnLocation to avoid reload problem
    */
   header("Location: " . $returnLocation);
-?>
