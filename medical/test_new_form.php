fbe1af7d1d464dcbce506ccb2d5ed12c5ad4d3c8
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 22:07:44 +0200

    La clase Error cambia de nombre a AppError porque en PHP7 hay una clase predefinida con ese mismo nombre (#9)

--- medical/test_new_form.php
+++ medical/test_new_form.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2008 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: test_new_form.php,v 1.32 2008/03/23 12:00:18 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -82,7 +81,7 @@
   echo $patient->getHeader();
   echo $problem->getHeader();
 
-  //Error::debug($formVar);
+  //AppError::debug($formVar);
 
   echo Form::errorMsg();
 
@@ -116,4 +115,3 @@
   Form::unsetSession();
 
   require_once("../layout/footer.php");
-?>
