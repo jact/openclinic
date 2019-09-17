96ccec92d08a75aa95d5f76ec552fcc93ae9f983
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 23:53:08 +0200

    La clase Error cambia de nombre a AppError porque en PHP7 hay una clase predefinida con ese mismo nombre (#9)

--- admin/user_pwd_reset_form.php
+++ admin/user_pwd_reset_form.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2008 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: user_pwd_reset_form.php,v 1.38 2008/09/28 20:19:30 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -68,11 +67,11 @@
       $formVar["login"] = $user->getLogin();
       $formVar["pwd"] = $formVar["pwd2"] = "";
       //$formVar["pwd"] = $formVar["pwd2"] = $user->getPwd(); // no because it's encoded
-      //Error::debug($user->getPwd());
+      //AppError::debug($user->getPwd());
     }
     else
     {
-      Error::fetch($userQ, false);
+      AppError::fetch($userQ, false);
     }
     $userQ->freeResult();
     $userQ->close();
@@ -157,4 +156,3 @@
   Form::unsetSession();
 
   require_once("../layout/footer.php");
-?>
