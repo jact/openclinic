611de86a46a87503d0454a40e00a926926a3c2a7
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 23:52:02 +0200

    La clase Error cambia de nombre a AppError porque en PHP7 hay una clase predefinida con ese mismo nombre (#9)

--- admin/staff_edit_form.php
+++ admin/staff_edit_form.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2008 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: staff_edit_form.php,v 1.33 2008/03/23 11:58:56 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -76,7 +75,7 @@
     }
     else
     {
-      Error::fetch($staffQ, false);
+      AppError::fetch($staffQ, false);
     }
     $staffQ->freeResult();
     $staffQ->close();
@@ -142,4 +141,3 @@
   Form::unsetSession();
 
   require_once("../layout/footer.php");
-?>
