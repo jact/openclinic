22cfeb7361817a6eaa148661f6f16396cd438d95
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 22:16:03 +0200

    La clase Error cambia de nombre a AppError porque en PHP7 hay una clase predefinida con ese mismo nombre (#9)

--- doc/index.php
+++ doc/index.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2008 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: index.php,v 1.24 2008/03/23 11:59:12 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -67,7 +66,6 @@
 
   echo HTML::section(1, $title);
 
-  Error::trace($_GET); // debug
+  AppError::trace($_GET); // debug
 
   require_once("../layout/footer.php");
-?>
