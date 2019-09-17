2c63cf2d797a3cf9861f546b6886dcc9427073c7
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-17 22:04:59 +0200

    Cambia el nombre del constructor de la clase (PHP7) (#9)

--- model/Theme.php
+++ model/Theme.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2013 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: Theme.php,v 1.17 2013/01/19 10:26:59 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -63,7 +62,7 @@ class Theme
 
   private $_trans; // to translate htmlspecialchars()
 
-  public function Theme()
+  public function __construct()
   {
     $this->_trans = array_flip(get_html_translation_table(HTML_SPECIALCHARS));
   }
@@ -278,4 +277,3 @@ class Theme
     return $this->getName();
   }
 } // end class
-?>
