06b6fa4c0935545080c7297303b23a2c42bbeed8
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-28 10:32:12 +0200

    La clase String cambia de nombre a StringLib porque en PHP7 hay una clase predefinida con ese mismo nombre (#9)

--- medical/relative_search.php
+++ medical/relative_search.php
@@ -7,9 +7,8 @@
  * Licensed under the GNU GPL. For full terms see the file LICENSE.
  *
  * @package   OpenClinic
- * @copyright 2002-2013 jact
+ * @copyright 2002-2019 jact
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @version   CVS: $Id: relative_search.php,v 1.46 2013/01/13 14:24:55 jact Exp $
  * @author    jact <jachavar@gmail.com>
  */
 
@@ -202,7 +201,7 @@
   {
     $row = $patQ->getCurrentRow() . '.';
     $row .= Form::checkBox("check[]", $pat->getIdPatient(),
-      array('id' => String::numberToAlphabet($patQ->getCurrentRow()))
+      array('id' => StringLib::numberToAlphabet($patQ->getCurrentRow()))
     );
     $row .= OPEN_SEPARATOR;
 
